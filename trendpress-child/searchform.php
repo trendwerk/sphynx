27e8a29c55be3f89d5b8cd3e3fc4a6bad1592832
Harold Angenent <harold@inten-games.com>
2012-07-19 10:55:03 +0200

    Parent theme only
    The repository was split - this only contains the parent theme now.

--- trendpress-child/searchform.php
+++
@@ -1,8 +0,0 @@
-<div class="search">
-	<form action="<?php echo get_option('siteurl'); ?>" method="get">
-	    <fieldset>
-	        <input type="text" name="s" value="<?php _e('Keyword','tp'); ?>&hellip;" />
-	        <input type="submit" value="<?php _e('Go','tp');?>" />
-	    </fieldset>
-	</form>
-</div>
