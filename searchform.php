f42be9b2152ab577161a6d33280aa56a8af9a1d7
Gewoonderrik <derrikwolthuis@gmail.com>
2012-12-20 16:38:40 +0100

    Placed placeholder instead of value in search
    Joy to the world!

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <div class="search">
 	<form action="<?php echo get_option('siteurl'); ?>" method="get">
 	    <fieldset>
-	        <input type="text" name="s" value="<?php _e('Search','tp'); ?>&hellip;" />
+	        <input type="text" name="s" placeholder="<?php _e('Search','tp'); ?>&hellip;" />
 	        <input type="submit" value="<?php _e('Search','tp');?>" />
 	    </fieldset>
 	</form>
