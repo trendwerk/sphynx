d4c3ce6191936143be9f52a5474f355d693861bb
Harold Angenent <harold@inten-games.com>
2013-03-12 17:33:57 +0100

    Obsessive stuff

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@
 /**
  * @includes Include the following PHP files
  */
- 
 include('assets/inc/functions.menus.php');
 include('assets/inc/functions.sidebars.php');
 include('assets/inc/functions.widgets.php');
@@ -24,12 +23,10 @@ function tp_enqueue_scripts() {
 	wp_enqueue_script('less');
 	wp_enqueue_style('less-to-css');
 }
-
 add_action('wp_enqueue_scripts','tp_enqueue_scripts');
 
 /**
  * @languages Add the language domain
  */
 load_theme_textdomain('tp',STYLESHEETPATH.'/assets/lang');
-
 ?>
