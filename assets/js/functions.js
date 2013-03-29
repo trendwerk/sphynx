72fcd220d69dc16e51e46a8dce3910174cf9f28b
Derrik <derrik@trendwerk.nl>
2013-03-29 16:59:46 +0100

    Improved JS
    * Removed old includes
    * Removed derpy comments

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,59 +1,43 @@
-/*
-
-	FUNCTIONS.JS (CHILD)
-	
-	All the jQuery/JavaScript enchangements go here...
-	
-	------------------------------------------------------------------	
-	
-	FILE INFO
-	
-	Last update: 	06/07/2012
-	Update by:		Derrik Wolthuis
-	
-	------------------------------------------------------------------
-	
-	INDEX
-	
-	1.	TEMPLATE URL
-	2.	RESPONSIVE
-	3.	FANCYBOX
-	4.	CYCLE
-	5.	MODERNIZR
-		
-	------------------------------------------------------------------
-	
-*/
-
 jQuery(document).ready(function($){
-						
-	// 1. TEMPLATE URL IS DEFINED THROUGH A JQUERY CONSTANT (not really a constant but it's quite constant)
-	
+
+	/**
+	 * @parent-theme-functions Load the functions.js from the TrendPress parent theme
+	 */
+							
 	$.getScript(templateurl+'/assets/js/functions.js');	    
-	
-	//	2. RESPONSIVE
-	
-	/*	see if the body class is g960... if so, then turn our
-		main navigation in a select list if on small screen
-		widths. also remove some base width and height image
-		settings for fluid images. */
-	
-		// remove element height and width for responsiveness
-			
-		$(window).load(function() {
-	
-			$('.wp-caption').removeAttr('style');
-	
-			var pic = $('img');
-	
-				pic.removeAttr('width'); 
-				pic.removeAttr('height');
-	
-		});
 		
-	// 3. FANCYBOX
+	/** 
+	 * @responsive Remove the width and height added to images by WordPress for responsive design
+	 */			
+	 
+	$(window).load(function() {
+
+		$('.wp-caption').removeAttr('style');
+
+		var pic = $('img');
+
+			pic.removeAttr('width'); 
+			pic.removeAttr('height');
+
+	});
 	
-	// if there's a a.fancybox, then get the fancybox script
+	/** 
+	 * @repsonsive Place sidebars after content for responsive design purposes
+	 */
+	
+	var bodyWidth = window.innerWidth;
+		
+	if (window.innerWidth < 1000 ) {
+				
+		$('#main .sidebar').insertAfter('#main #content');
+		
+		$('.widget:even').addClass('clear');
+
+	} 
+		
+	/** 
+	 * @fancybox
+	 */		
 		
 	var thumbnails = jQuery('a:has(img)').filter( function() { 
 				
@@ -74,57 +58,5 @@ jQuery(document).ready(function($){
 	  }
 	  
 	});
-					
-	if($('a.fancybox').length > 0) {
-			
-		$('a.fancybox').fancybox();
-		
-	    jQuery.getCSS = function( url, media ) {
-	            jQuery( document.createElement('link') ).attr({
-	                    href: templateurl+'/assets/js/fancybox/jquery.fancybox.css',
-	                    media: media || 'screen',
-	                    type: 'text/css',
-	                    rel: 'stylesheet'
-	            }).appendTo('head');
-	    };
-			
-	    $.getCSS();
-			
-	}
-			
-	// 4. CYCLE 
-	
-	/* 	see if there's an #cycle-slider. if true, then load
-		cycle.js and add a progressie enhancement navigation
-		and pagination */
-		
-	if($('#cycle-slider').length > 0) {
-	
-		$('#cycle-slider').cycle({
-
-			fx: 'scrollHorz', 
-			speed: 'fast', 
-			timeout: 7000,
-			pager: '#cycle-pager-inner',
-			prev: '#prev', 
-			next: '#next'	
-	
-		});	
-	
-		$('div#cycle-slider').after('<div id="cyle-nav"><a id="prev" href="#">Vorige</a><a id="next" href="#">Volgende</a></div>');
-		$('div#cycle-slider').after('<div id="cycle-pager"><div id="cycle-pager-inner">');
-	
-	}
-	
-	var bodyWidth = window.innerWidth;
-		
-	if (window.innerWidth < 1000 ) {
-				
-		$('#main .sidebar').insertAfter('#main #content');
-		
-		$('.widget:even').addClass('clear');
-
-	} 
-	
-		
+						
 });
