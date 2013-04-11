0e66e29355d3bfc2465a5dd0946303e355e9d471
Harold Angenent <harold@inten-games.com>
2013-04-11 13:14:42 +0200

    Sidebar OO

--- assets/inc/functions.sidebars.php
+++ assets/inc/functions.sidebars.php
@@ -5,19 +5,18 @@
  */
 
 function tp_register_sidebars() {
-	tp_register_sidebar('home',array(
+	new TPSidebar('home',array(
 		'name' => __('Home','tp')
 	));
-	tp_register_sidebar('page',array(
+	new TPSidebar('page',array(
 		'name' => __('Page','tp')
 	));
-	tp_register_sidebar('blog',array(
+	new TPSidebar('blog',array(
 		'name' => __('Blog','tp')
 	));
-	tp_register_sidebar('footerid',array(
+	new TPSidebar('footerid',array(
 		'name' => __('Footer','tp')
 	));
 }
 add_action('init','tp_register_sidebars');
-
 ?>
