75198789be6553625ff045133404a056ee10f35f
Derrik <derrik@trendwerk.nl>
2013-04-11 16:48:12 +0200

    Added fancybox back to functions. Improving tables.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -48,6 +48,19 @@ jQuery(document).ready(function($){
 	  
 	});
 	
+	$('.fancybox').fancybox({
+		   
+		openEffect	: 'elastic',
+    	closeEffect	: 'elastic',
+
+    	helpers : {
+    		title : {
+    			type : 'inside'
+    		}
+    	}	
+		
+	});
+	
 	/**
 	 * @mainnav Hover state
 	 */
