aea0e0803d993826825aa22de39b0b912f95193e
Derrik <derrik@trendwerk.nl>
2013-04-11 16:18:18 +0200

    Changed title id's and classes, added share.php

---
+++ share.php
@@ -0,0 +1,11 @@
+<div class="share">
+	<div class="addthis_toolbox addthis_default_style">
+		<a class="addthis_button_preferred_1"></a>
+		<a class="addthis_button_preferred_2"></a>
+		<a class="addthis_button_preferred_3"></a>
+		<a class="addthis_button_preferred_4"></a>
+		<a class="addthis_button_compact"></a>
+		<a class="addthis_counter addthis_bubble_style"></a>
+	</div>
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-5166bebc0496c543"></script>
+</div>
