206f3b59fff04269e7c65b41a4f73f6f0d2bc0ca
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2013-04-11 16:31:00 +0200

    Revert "Functions.js in parent en child opbouwen volgens wp guidelines"
    This reverts commit 537f63fb2c4e383e01299c656c6f616f67969e26.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,14 +3,13 @@ jQuery(document).ready(function($){
 	/**
 	 * @gallery Remove unnecessary style, add rel gallery for fancybox plug-in
 	 */
-	 
 	$('.gallery a').attr('rel', 'gallery');
+
 	$('.gallery br').removeAttr('style');
 	
 	/**
 	 * @pseudo-classes Add pseudo-class classes
 	 */
-	 
 	$('#mainnav ul.sf-menu li:first-child').addClass('first-child');
 	$('#mainnav ul.sf-menu li:last-child').addClass('last-child');
 
@@ -26,36 +25,46 @@ jQuery(document).ready(function($){
 	/**
 	 * @zebra-tables
 	 */
-	 
 	if ($('body').has('table')) {
+
 		$('table tr:odd').addClass('odd');	
 		$('table tr:even').addClass('even');
+		
 		$('section table tr td:last-child').addClass('last-child');
-	};
+
+	}
 
 	/**
 	 * @external-links Add class external to external links and open rel="external" in a new tab
 	 */
-	 
 	$('a').filter(function() {
+
 		return this.hostname && this.hostname !== location.hostname;
+
 	}).addClass('external');
+ 
 	$('a[rel=external]').each(function(i){
+
 		this.target='_blank';
+		
 	});
 	
 	/**
 	 * @browser-detection Adds the current browser, OS and render-engine to body class
 	 */
-	 
 	$.each($.browser, function(i) {
 	    $('body').addClass(i);
 	    return false;  
 	});
+	
 	var os = ['iphone', 'ipad', 'windows', 'mac', 'linux'];
+	
 	var match = navigator.appVersion.toLowerCase().match(new RegExp(os.join('|')));
+	
 	if (match) {
+	
 	    $('body').addClass(match[0]);
+	
 	};
 
 });
