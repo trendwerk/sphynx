3049055fd204cb27e68f1eb47fa749ea1608f1e7
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2013-06-16 19:06:45 +0200

    Add hide profile feature

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -79,7 +79,7 @@ jQuery(document).ready(function($){
 	
 	$('#content article:last').addClass('last-child');
 	
-	 /*
+	/*
      * @misc Replace all SVG images with inline SVG
      */
     jQuery('img.svg').each(function(){
