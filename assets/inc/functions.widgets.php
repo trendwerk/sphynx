a66133118b4f8845d0c2702512e3a3089ab44f26
Harold Angenent <harold@trendwerk.nl>
2013-10-08 16:44:56 +0200

    Added Twitter widget

--- assets/inc/functions.widgets.php
+++ assets/inc/functions.widgets.php
@@ -388,6 +388,64 @@ class tp_fb_like_box extends WP_Widget {
 add_action('widgets_init',create_function('','return register_widget("tp_fb_like_box");'));
 
 /**
+ * @widget Tweets from a Twitter account
+ */
+class tp_tweets extends WP_Widget {
+	function tp_tweets() {
+		$this->WP_Widget( 'tp_tweets', __( 'Tweets', 'tp' ), 'description=' . __( 'Shows latest tweets from a Twitter account', 'tp' ) );
+	}
+	
+	function form( $instance ) {
+		$title = esc_attr( isset( $instance['title'] ) ? $instance['title'] : '' );
+		$username = isset( $instance['username'] ) ? $instance['username'] : '';
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">
+				<strong><?php _e( 'Title' ); ?></strong><br />
+				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
+			</label>
+		</p>
+		
+		<p>
+			<label>
+				<strong><?php _e( 'Twitter username', 'tp' ); ?></strong><br />
+				<input class="widefat" name="<?php echo $this->get_field_name( 'username' ); ?>" type="text" value="<?php echo $username; ?>" />
+			</label>
+		</p>
+		<?php
+	}
+	
+	function update($new_instance,$old_instance) {
+		$instance = $old_instance;
+		
+		$instance['title'] = esc_attr( $new_instance['title'] );
+		$instance['username'] = $new_instance['username'];
+				
+		return $instance;
+	}
+	
+	function widget($args,$instance) {		
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		$username = $instance['username'];
+		extract( $args );
+		
+		echo $before_widget;
+			if( $title )
+				echo $before_title . $title . $after_title;
+			?>
+			<a class="twitter-timeline" data-chrome="noheader nofooter noscrollbar transparent" data-tweet-limit="3" data-dnt="true" href="https://twitter.com/<?php echo $username; ?>" data-screen-name="<?php echo $username; ?>" data-widget-id="387581823514972160">
+				<?php printf( __( 'Tweets from @%1$s', 'tp' ), $username ); ?>
+			</a>
+			
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+			<?php
+			
+		echo $after_widget;
+	}
+}
+add_action( 'widgets_init', create_function( '', 'return register_widget("tp_tweets");' ) );
+
+/**
  * @widget Text with button
  */
 class widget_title_content_button extends WP_Widget {
