91b306c3338e5d5151cbd25a75408b19996626d7
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2013-11-01 13:41:47 +0100

    applied coding standards

--- assets/inc/functions.misc.php
+++ assets/inc/functions.misc.php
@@ -2,28 +2,28 @@
 /**
  * @editor add editor styles
  */
-function tp_add_editor_styles($settings) {
+function tp_add_editor_styles( $settings ) {
     $style_formats = array(
     	array(
-    		'title' => __('Primary button','tp'),
+    		'title'    => __( 'Primary button', 'tp' ),
     		'selector' => 'a',
-    		'classes' => 'cta primary'
+    		'classes'  => 'cta primary'
     	),
     	array(
-    		'title' => __('Secondary button','tp'),
+    		'title'    => __( 'Secondary button', 'tp' ),
     		'selector' => 'a',
-    		'classes' => 'cta secondary'
+    		'classes'  => 'cta secondary'
     	),
     	array(
-    		'title' => __('More link','tp'),
+    		'title'    => __( 'More link', 'tp' ),
     		'selector' => 'a',
-    		'classes' => 'more-link'
+    		'classes'  => 'more-link'
     	)
     );
-    $settings['style_formats'] = json_encode($style_formats);
+    $settings['style_formats'] = json_encode( $style_formats );
     return $settings;
 }
-add_filter('tiny_mce_before_init','tp_add_editor_styles');
+add_filter( 'tiny_mce_before_init', 'tp_add_editor_styles' );
 
 /**
  * @thumbnail set image sizes
@@ -31,32 +31,32 @@ add_filter('tiny_mce_before_init','tp_add_editor_styles');
 function tp_set_image_sizes() {
 
 	/* set thumbnail size */
-	update_option('thumbnail_size_w', 140);
-	update_option('thumbnail_size_h', 140);
+	update_option( 'thumbnail_size_w', 140 );
+	update_option( 'thumbnail_size_h', 140 );
 	
 	/* set medium image size */
-	update_option('medium_size_w', 300);
-	update_option('medium_size_h', '');
+	update_option( 'medium_size_w', 300 );
+	update_option( 'medium_size_h', '' );
 	
 	/* set large image size */
-	update_option('large_size_w', 600);
-	update_option('large_size_h', '');
+	update_option( 'large_size_w', 600 );
+	update_option( 'large_size_h', '' );
 }
 add_action('after_switch_theme', 'tp_set_image_sizes'); 
 
 /**
  * @author Remove AIM, YIM, JABBER and add Facebook and LinkedIn
  */
-function tp_modify_profile($media) {
-	unset($media['aim']);
-	unset($media['yim']);
-	unset($media['jabber']);
-	unset($media['googleplus']);
+function tp_modify_profile( $media ) {
+	unset( $media['aim'] );
+	unset( $media['yim'] );
+	unset( $media['jabber'] );
+	unset( $media['googleplus'] );
 
-	$media['facebook'] = __('Facebook profile URL','tp');
-	$media['linkedin'] = __('LinkedIn profile URL','tp');
-	$media['googleplus'] = __('Google+ profile URL','tp');
+	$media['facebook']   = __( 'Facebook profile URL', 'tp' );
+	$media['linkedin']   = __( 'LinkedIn profile URL', 'tp' );
+	$media['googleplus'] = __( 'Google+ profile URL', 'tp' );
 	
 	return $media;
 }
-add_filter('user_contactmethods','tp_modify_profile');
+add_filter( 'user_contactmethods','tp_modify_profile' );
--- assets/inc/functions.posttypes.php
+++ assets/inc/functions.posttypes.php
@@ -7,11 +7,11 @@ function tp_register_post_types() {
 	/**
 	 * @support Thumbnails
 	 */
-	add_theme_support('post-thumbnails');
-	remove_post_type_support('page','thumbnail');
+	add_theme_support( 'post-thumbnails' );
+	remove_post_type_support( 'page', 'thumbnail' );
 	
 	/**
 	 * @register Post types and taxonomies
 	 */
 }
-add_action('init','tp_register_post_types');
+add_action( 'init', 'tp_register_post_types' );
