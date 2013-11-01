4b0bd62e301ab5db539d151500c192dcf618dcd1
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2013-11-01 14:02:32 +0100

    Updated recommended plugins

--- assets/inc/admin-views/recommended-plugins.php
+++ assets/inc/admin-views/recommended-plugins.php
@@ -1,14 +1,22 @@
 <div class="wrap">
+
 	<div id="icon-plugins" class="icon32"><br /></div>
+
 	<h2><?php _e('Recommended plugins','tp'); ?></h2>
 	
-	<p><?php _e('On this page, you can install recommended plugins and apply recommended settings to them after installing.','tp'); ?></p>
+	<p><?php _e('On this page, you can install plugins recommended by Trendwerk and apply recommended settings to them after installing.','tp'); ?></p>
 	
 	<h3><?php _e('Recommended plugins','tp'); ?></h3>
 	<?php $this->show_plugins($this->recommended); ?>
 	
-	<p>&nbsp;</p>
+	<br />
 	
 	<h3><?php _e('Optional plugins','tp'); ?></h3>
 	<?php $this->show_plugins($this->optional); ?>	
+
+	<br />
+	
+	<h3><?php _e('Developer tools','tp'); ?></h3>
+	<?php $this->show_plugins($this->development); ?>	
+
 </div>
