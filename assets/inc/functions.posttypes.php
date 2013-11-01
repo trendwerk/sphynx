226b921f2db76e57fb7694cbd8ea81ec76cefdb9
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2013-11-01 13:44:32 +0100

    Revert "Removed useless comment"
    This reverts commit 698d87c8d33f9ae40155a61b795cd412406b6b9a.

--- assets/inc/functions.posttypes.php
+++ assets/inc/functions.posttypes.php
@@ -10,5 +10,8 @@ function tp_register_post_types() {
 	add_theme_support( 'post-thumbnails' );
 	remove_post_type_support( 'page', 'thumbnail' );
 	
+	/**
+	 * @register Post types and taxonomies
+	 */
 }
 add_action( 'init', 'tp_register_post_types' );
