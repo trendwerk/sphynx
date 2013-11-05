d64b3baecd7abb1141a8963acd9a160007feab9b
Harold Angenent <harold@trendwerk.nl>
2013-11-05 17:37:27 +0100

    Fixes #13

--- assets/inc/class-tp-widgets.php
+++ assets/inc/class-tp-widgets.php
@@ -36,4 +36,22 @@ class TP_Widgets {
 
 /**
  * Static widgets
+ * 
+ * @param string $class The class name of the widget
  */
+function tp_static_widget( $class, $args = array(), $instance = array() ) {
+	if( ! class_exists( $class ) )
+		return;
+
+	$widget = new $class;
+
+	$default = array(
+		'before_widget' => '<div class="widget ' . $widget->widget_options['classname'] . '">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widgettitle">',
+		'after_title'   => '</h3>',
+	);
+	$args = wp_parse_args( $args, $default );
+
+	$widget->widget( $args, $instance );
+}
