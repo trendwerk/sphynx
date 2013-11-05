27addc01c702a8083d8272e4a383500c56a9fd99
Harold Angenent <harold@trendwerk.nl>
2013-11-05 15:50:25 +0100

    Coding standards stuff

--- assets/inc/class.TPPlugins.php
+++ assets/inc/class.TPPlugins.php
@@ -6,79 +6,84 @@
  * Name it and let's go!
  */
 
-class TPPlugins {
+class TP_Plugins {
 	var $plugins;
 	
 	function __construct() {
 		$this->activate();
 		
-		if(count($this->plugins)) :
-			add_action('admin_enqueue_scripts',array($this,'add_scripts'));
-			add_action('views_plugins',array($this,'add_tab'));
+		if( 0 < count( $this->plugins ) ) {
+			add_action( 'admin_enqueue_scripts', array( $this, 'add_scripts' ) );
+			add_action( 'views_plugins', array( $this, 'add_tab' ) );
 			
-			if(isset($_GET['plugin_status'])) :
-				if($_GET['plugin_status'] == 'tp') :
-					add_action('admin_head',array($this,'set_status'));
-					add_filter('all_plugins',array($this,'show'));
-					add_filter('plugin_action_links',array($this,'remove_actions'));
-					add_filter('manage_plugins_columns',array($this,'remove_checkbox'));
-					add_filter('bulk_actions-plugins',array($this,'remove_bulk'));
-					add_filter('admin_body_class',array($this,'set_class'));
-				endif;
-			endif;
-		endif;
+			if( isset( $_GET['plugin_status'] ) ) {
+				if( 'tp' == $_GET['plugin_status'] ) {
+					add_action( 'admin_head', array( $this, 'set_status' ) );
+					add_filter( 'all_plugins', array( $this, 'show' ) );
+					add_filter( 'plugin_action_links', array( $this, 'remove_actions' ) );
+					add_filter( 'manage_plugins_columns', array( $this, 'remove_checkbox' ) );
+					add_filter( 'bulk_actions-plugins', array( $this, 'remove_bulk' ) );
+					add_filter( 'admin_body_class', array( $this, 'set_class' ) );
+				}
+			}
+		}
 	}
 	
 	/**
 	 * Add scripts
 	 */
 	function add_scripts() {
-		wp_enqueue_script('tp-plugins',get_template_directory_uri().'/assets/js/TPPlugins/TPPlugins.js');
+		wp_enqueue_script( 'tp-plugins', get_template_directory_uri() . '/assets/js/TPPlugins/TPPlugins.js' );
 		
 		$theme = wp_get_theme();		
-		wp_localize_script('tp-plugins','TPPluginsL10n',array(
-			'tp-plugins-explanation' => sprintf(__('Plugins in the %1$s folder are executed automatically.','tp'),'<code>'.$theme->get_stylesheet().'/assets/plugins/</code>')
-		));
+		wp_localize_script( 'tp-plugins', 'TPPluginsL10n', array(
+			'tp-plugins-explanation' => sprintf( __( 'Plugins in the %1$s folder are executed automatically.', 'tp' ), '<code>' . $theme->get_stylesheet() . '/assets/plugins/</code>' )
+		) );
 	}
 	
 	/**
 	 * Set body class for futher jQuery adjustments
 	 */
-	function set_class($classes) {
-		return $classes.' tp-plugins-current';
+	function set_class( $classes ) {
+		return $classes . ' tp-plugins-current';
 	}
 	
 	/**
 	 * Set status to our status
 	 */
 	function set_status() {
-		global $status,$totals;
+		global $status, $totals;
 		$status = 'tp';
-		$totals = wp_parse_args($this->_totals,$totals);
+		$totals = wp_parse_args( $this->_totals, $totals );
 	}
 	
 	/**
 	 * Show our plugins instead of WP's
 	 */
-	function show($plugins) {
+	function show( $plugins ) {
 		//Save old totals
-		$this->_totals = array('all' => 0,'active' => 0,'inactive' => 0);
-		foreach($plugins as $file=>$plugin) :
+		$this->_totals = array( 
+			'all'      => 0,
+			'active'   => 0,
+			'inactive' => 0
+		);
+
+		foreach( $plugins as $file => $plugin ) {
 			$this->_totals['all']++;
-			if(is_plugin_active($file) || is_plugin_active_for_network($file)) :
+
+			if( is_plugin_active( $file ) || is_plugin_active_for_network( $file ) )
 				$this->_totals['active']++;
-			else :
+			else
 				$this->_totals['inactive']++;
-			endif;
-		endforeach;
+		}
 	
 		//Setup our plugins
 		$plugins = array();
-		if($this->plugins) :
-			foreach($this->plugins as $plugin) :
-				$plugins[$plugin] = get_plugin_data($plugin);
-			endforeach;
-		endif;
+		if( $this->plugins ) {
+			foreach( $this->plugins as $plugin ) {
+				$plugins[ $plugin ] = get_plugin_data( $plugin );
+			}
+		}
 		
 		return $plugins;
 	}
@@ -86,7 +91,7 @@ class TPPlugins {
 	/**
 	 * Remove actions
 	 */
-	function remove_actions($actions) {
+	function remove_actions( $actions ) {
 		return;
 	}
 	
@@ -100,7 +105,7 @@ class TPPlugins {
 	/**
 	 * Remove checkboxes
 	 */
-	function remove_checkbox($columns) {
+	function remove_checkbox( $columns ) {
 		$columns['cb'] = '';
 				
 		return $columns;
@@ -109,12 +114,12 @@ class TPPlugins {
 	/**
 	 * Add another tab to WordPress "Installed Plugins"
 	 */
-	function add_tab($views) {
+	function add_tab( $views ) {
 		global $status;
 		
-		$class = ($status == 'tp') ? 'current' : '';
+		$class = ( $status == 'tp' ) ? 'current' : '';
 		
-		$views['tp'] = '<a class="'.$class.'" href="'.admin_url('plugins.php?plugin_status=tp').'">'.__('TrendPress','tp').' <span class="count">('.count($this->plugins).')</span></a>';
+		$views['tp'] = '<a class="' . $class . '" href="' . admin_url('plugins.php?plugin_status=tp') . '">' . __('TrendPress','tp') . ' <span class="count">(' . count( $this->plugins ) . ')</span></a>';
 		
 		return $views;
 	}
@@ -123,11 +128,11 @@ class TPPlugins {
 	 * Activate them plugins
 	 */
 	function activate() {
-		if($this->plugins = $this->get_plugins()) :
-			foreach($this->plugins as $plugin) :
-				include_once($plugin);
-			endforeach;
-		endif;
+		if( $this->plugins = $this->get_plugins() ) {
+			foreach( $this->plugins as $plugin ) {
+				include_once( $plugin );
+			}
+		}
 	}
 	
 	/**
@@ -135,13 +140,35 @@ class TPPlugins {
 	 */
 	function get_plugins() {
 		$plugins = array();
-		$folder = STYLESHEETPATH.'/assets/plugins/';
-		if(is_dir($folder)) :
-			foreach(scandir($folder) as $plugin) :
-				if(substr($plugin,0,1) == '.') continue;
+		$folder = STYLESHEETPATH . '/assets/plugins/';
+
+		if( is_dir( $folder ) ) :
+			foreach( scandir( $folder ) as $plugin ) :
+				if( '.' == substr( $plugin, 0, 1 ) ) continue;
 				
-				$plugin = $folder.$plugin.'/'.$plugin.'.php';
-				if(!is_readable($plugin)) continue;
+				/**
+				 * Determine plugin dir
+				 */
+				$plugin_dir = $folder . $plugin;
+				
+				/**
+				 * Determine plugin file
+				 */
+				if( is_dir( $plugin_dir ) ) {
+					foreach( scandir( $plugin_dir ) as $plugin_file ) {
+						if( '.' == substr( $plugin_file, 0, 1 ) ) continue;
+						if( substr( $plugin_file, -4 ) != '.php' ) continue;
+
+						$plugin_data = get_plugin_data( $plugin_dir . '/' . $plugin_file, false, false );
+
+						if( empty( $plugin_data['Name'] ) )
+							continue;
+
+						$plugin = $plugin_dir . '/' . $plugin_file;
+					}
+				}
+
+				if( ! is_readable( $plugin ) ) continue;
 				
 				$plugins[] = $plugin;
 			endforeach;
@@ -149,5 +176,4 @@ class TPPlugins {
 		
 		return $plugins;
 	}
-} new TPPlugins;
-?>
+} new TP_Plugins;
