4c53bb01e1c55956e50c8bd2939227977f40e37c
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2013-11-11 22:40:00 +0100

    Added http status code 503 to parent theme

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php _e('Please activate a TrendPress child-theme to use this theme.','tp'); ?>
+<?php
+	$protocol = "HTTP/1.0";
+	if ( "HTTP/1.1" == $_SERVER["SERVER_PROTOCOL"] )
+	  $protocol = "HTTP/1.1";
+	header( "$protocol 503 Service Unavailable", true, 503 );
+	header( "Retry-After: 600" );
+?>
+<div style="font: 13px/20px sans-serif; margin: 10%; text-align: center;">
+	<?php _e('Please activate a TrendPress child-theme to use this theme.','tp'); ?>
+</div>
