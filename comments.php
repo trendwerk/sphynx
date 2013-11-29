3a494d0eeaf10bb454fad848388514dc56ec5eb8
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2013-11-29 15:36:17 +0100

    re-added changes to comments.php

--- comments.php
+++ comments.php
@@ -8,7 +8,7 @@
     <?php if ( have_comments() ) : ?>
     
         <h2 class="comments-title">
-            <?php printf( _n( 'One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'tp' ), number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' ); ?>
+            <?php printf( _n( 'One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'tp' ), get_comments_number(), '<span>' . get_the_title() . '</span>' ); ?>
         </h2>
         
         <ol class="commentlist">
