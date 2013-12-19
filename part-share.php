90db1e0ab3e21e2bf68b103a5dfaa4666a4a82cb
Derrik Wolthuis <derrik@trendwerk.nl>
2013-12-19 16:50:32 +0100

    Made share thingy

--- part-share.php
+++ part-share.php
@@ -1,12 +1,35 @@
 <section class="share">
 
-	<div class="addthis_toolbox addthis_default_style ">
-		<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-		<a class="addthis_button_tweet"></a>
-		<a class="addthis_button_pinterest_pinit"></a>
-		<a class="addthis_counter addthis_pill_style"></a>
-	</div>
+	<p class="addthis_toolbox">
+
+		<span class="label"><?php _e( 'Share this article', 'tp' ); ?>:</span>
+
+		<a title="<?php _e('Share this on Facebook','tp'); ?>" class="addthis_button_facebook">
+			<i class="icon-facebook"></i>
+		</a>
+
+		<a title="<?php _e('Share this on Twitter','tp'); ?>" class="addthis_button_twitter">
+			<i class="icon-twitter"></i>
+		</a>
+
+		<a title="<?php _e('Share this on LinkedIn','tp'); ?>" class="addthis_button_linkedin">
+			<i class="icon-linkedin"></i>
+		</a>
+		
+		<a title="<?php _e('Share this on Google Plus','tp'); ?>" class="addthis_button_google_plusone_share">
+			<i class="icon-google-plus"></i>
+		</a>
+
+		<a title="<?php _e('Share this on Pinterest','tp'); ?>" class="addthis_button_pinterest_share">
+			<i class="icon-pinterest"></i>
+		</a>
+		
+		<a title="<?php _e('Email this','tp'); ?>" class="addthis_button_email">
+			<i class="icon-envelope"></i>
+		</a>
+
+	</p>
 
 	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51a880a417084aa1"></script>
-	
+
 </section>
