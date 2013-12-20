154cbac11132c8236f2e565d43cef24a32e12a6d
Harold Angenent <harold@intengames.com>
2013-12-20 12:30:31 +0100

    Even more code consistency

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <?php
-	/**
-	 * TrendPress Maintenance mode
-	 * @see wp_maintenance() in wp-includes/load.php
-	 *
-	 * @package TrendPress
-	 */
-	
-	$protocol = $_SERVER["SERVER_PROTOCOL"];
-	if ( 'HTTP/1.1' != $protocol && 'HTTP/1.0' != $protocol )
-		$protocol = 'HTTP/1.0';
-	header( "$protocol 503 Service Unavailable", true, 503 );
-	header( 'Content-Type: text/html; charset=utf-8' );
-	header( 'Retry-After: 600' );
+/**
+ * TrendPress Maintenance mode
+ * @see wp_maintenance() in wp-includes/load.php
+ *
+ * @package TrendPress
+ */
+
+$protocol = $_SERVER["SERVER_PROTOCOL"];
+if ( 'HTTP/1.1' != $protocol && 'HTTP/1.0' != $protocol )
+	$protocol = 'HTTP/1.0';
+header( "$protocol 503 Service Unavailable", true, 503 );
+header( 'Content-Type: text/html; charset=utf-8' );
+header( 'Retry-After: 600' );
 ?>
-	<!DOCTYPE html>
-	<html xmlns="http://www.w3.org/1999/xhtml"<?php if ( is_rtl() ) echo ' dir="rtl"'; ?>>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title><?php _e( 'Maintenance' ); ?></title>
-	</head>
-		<body>
-			<h1><?php _e( 'Briefly unavailable for scheduled maintenance. Check back in a minute.' ); ?></h1>
-		</body>
-	</html>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"<?php if ( is_rtl() ) echo ' dir="rtl"'; ?>>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title><?php _e( 'Maintenance' ); ?></title>
+</head>
+	<body>
+		<h1><?php _e( 'Briefly unavailable for scheduled maintenance. Check back in a minute.' ); ?></h1>
+	</body>
+</html>
+
 <?php
-	die();
+die();
