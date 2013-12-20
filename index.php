343d000a570f21d4b0ac457d1d5c3f0e84c26970
Stagiair <luuk@trendwerk.nl>
2013-12-20 10:25:12 +0100

    Code consistency changes

--- index.php
+++ index.php
@@ -16,13 +16,12 @@
 	<!DOCTYPE html>
 	<html xmlns="http://www.w3.org/1999/xhtml"<?php if ( is_rtl() ) echo ' dir="rtl"'; ?>>
 	<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title><?php _e( 'Maintenance' ); ?></title>
-
 	</head>
-	<body>
-		<h1><?php _e( 'Briefly unavailable for scheduled maintenance. Check back in a minute.' ); ?></h1>
-	</body>
+		<body>
+			<h1><?php _e( 'Briefly unavailable for scheduled maintenance. Check back in a minute.' ); ?></h1>
+		</body>
 	</html>
 <?php
 	die();
