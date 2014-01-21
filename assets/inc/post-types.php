52c3e7d4749f08c95206ec193488068bfd051553
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2014-01-21 16:57:25 +0100

    Improved widget “Post types posts”

--- assets/inc/post-types.php
+++ assets/inc/post-types.php
@@ -12,6 +12,29 @@ function tp_register_post_types() {
 	remove_post_type_support( 'page', 'thumbnail' );
 
 	add_image_size( 'widget', 320, 500 );
+
+	/**
+	 * Testimonials
+	 *
+	 * @subpackage Post types
+	 */
+	$args = array(
+		'labels'            => array(
+			'name'          => __( 'Testimonials', 'tp' ),
+			'singular_name' => __( 'Testimonial', 'tp' ),
+			'add_new'       => __( 'Add testimonial', 'tp' ),
+			'add_new_item'  => __( 'Add new testimonial', 'tp' ),
+			'edit_item'     => __( 'Edit testimonial', 'tp' ),
+		),
+		'public'            => true,
+		'has_archive'       => true,
+		'menu_position'     => 5,
+		'rewrite'           => array(
+			'slug'          => __( 'testimonials', 'tp' ),
+		),
+		'supports'          => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+	); 
+	register_post_type( 'testimonials', $args );
 	
 }
 add_action( 'init', 'tp_register_post_types' );
--- assets/inc/widgets/class-tp-post-type-posts.php
+++ assets/inc/widgets/class-tp-post-type-posts.php
@@ -11,47 +11,82 @@ class TP_Post_Type_Posts extends WP_Widget {
 	}
 	
 	function form( $instance ) {
-		$post_types = get_post_types( array(
+		$post_types = array_diff( get_post_types( array(
 			'public'   => true,
-			'_builtin' => false,
-		), 'objects' );
+		) ), array( 'page', 'attachment' ) );
 
-		if( 0 < count( $post_types ) ) { 
+		if( 0 < count( $post_types ) ) {
 			?>
 
 			<p>
 				<label>
+					<strong><?php _e( 'Title', 'tp' ); ?></strong><br />
+					<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $instance['title']; ?>" />
+				</label>
+			</p>
 
-					<strong><?php _e( 'Post type' ); ?></strong>
-
+			<p>
+				<label>
+					<strong><?php _e( 'Post type', 'tp' ); ?></strong><br />
 					<select class="widefat" name="<?php echo $this->get_field_name( 'post_type' ); ?>">
-						<?php foreach( $post_types as $post_type ) { ?>
-							<option <?php selected( $post_type->name, $instance['post_type'] ); ?> value="<?php echo $post_type->name; ?>">
-								<?php echo $post_type->labels->name; ?>
-							</option>
-						<?php } ?>
-					</select>
+							<?php 
+								foreach( $post_types as $post_type ) { 
+									$post_type = get_post_type_object( $post_type ); 
+									?>
 
+									<option <?php selected( $post_type->name, $instance['post_type'] ); ?> value="<?php echo $post_type->name; ?>">
+										<?php echo $post_type->labels->name; ?>
+									</option>
+
+									<?php 
+								} 
+							?>
+						</select>
+				</label>
+			</p>
+
+			<p>
+				<label>
+					<?php _e( 'Number of posts to show:' ); ?>
+					<input name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $instance['number']; ?>" size="3" />
+				</label>
+			</p>
+
+			<p>
+				<label>
+					<input type="checkbox" name="<?php echo $this->get_field_name( 'archive_link' ); ?>" value="true" <?php checked( $instance['archive_link'], true ); ?>>
+					<?php _e( 'Show link to post type archive', 'tp' ); ?>
 				</label>
 			</p>
 
 			<?php 
 		}
 	}
-	
+
+	function update( $new_instance ) {
+		$new_instance['number'] = absint( $new_instance['number'] );
+		$new_instance['archive_link'] = isset( $new_instance['archive_link'] );
+		
+		return $new_instance;
+	}
+
 	function widget( $args, $instance ) {
 		extract( $args );
 		
 		$post_type = get_post_type_object( $instance['post_type'] );
 
 		$post_type_posts = new WP_Query( array(
-			'post_type'      => $post_type->name, 
-			'posts_per_page' => -1,
+			'post_type'           => $post_type->name, 
+			'posts_per_page'      => $instance['number'],
+			'ignore_sticky_posts' => true
 		) );
 	
 		if( $post_type_posts->have_posts() ) {
 			echo $before_widget;
-				echo $before_title . '<a href="' . get_post_type_archive_link( $post_type->name ) . '">' . $post_type->labels->name . '</a>' . $after_title;
+
+				if( $instance['title'] )
+				echo $before_title . $instance['title'] . $after_title;
+
 				?>
 		
 				<ul class="post-type-posts">
@@ -68,7 +103,15 @@ class TP_Post_Type_Posts extends WP_Widget {
 		
 				</ul>
 				
-				<?php
+				<?php if( $instance['archive_link'] ) { 
+					?>
+			    	
+		    		<a class="more-link" href="<?php echo get_post_type_archive_link( $post_type->name ); ?>">
+		    			<?php echo __( 'View all', 'tp') . ' ' . strtolower( $post_type->labels->name ); ?>
+		    		</a>
+
+		    		<?php 
+		    	} 
 			echo $after_widget;
 		}
 
