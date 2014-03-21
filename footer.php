63b7fe090de54ec22b657145fe2f61862ee5780d
Harold Angenent <harold@intengames.com>
2014-03-21 14:30:48 +0100

    Some more code consistency

--- footer.php
+++ footer.php
@@ -17,12 +17,12 @@
 					<nav id="footernav" class="navigation">
 						<?php 
 							wp_nav_menu( array(
-								'container' => '',
-								'fallback_cb' => 'false',
-								'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+								'container'      => '',
+								'fallback_cb'    => 'false',
+								'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
 								'theme_location' => 'footernav', 
-								'depth' => '1',
-							));
+								'depth'          => '1',
+							) );
 						?>
 					</nav>
 					
