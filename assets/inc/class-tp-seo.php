f927dd62fb85eabcd618bad43006d91d23f58c79
Harold Angenent <harold@intengames.com>
2014-04-18 15:08:04 +0200

    Dó index in case of doubt.

--- assets/inc/class-tp-seo.php
+++ assets/inc/class-tp-seo.php
@@ -7,7 +7,7 @@
 
 class TP_SEO {
 	function __construct() {
-		if( ! defined( 'TP_ENV' ) || 'release' == TP_ENV )
+		if( defined( 'TP_ENV' ) && 'release' == TP_ENV )
 			update_option( 'blog_public', 0 );
 	}
 } new TP_SEO;
