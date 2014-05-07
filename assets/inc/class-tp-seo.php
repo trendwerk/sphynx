0efbf6b937e2475e48ba4913e4d4e5acc793f7d3
Harold Angenent <harold@intengames.com>
2014-05-07 10:16:36 +0200

    Automatically allow indexation on master environment

--- assets/inc/class-tp-seo.php
+++ assets/inc/class-tp-seo.php
@@ -7,7 +7,9 @@
 
 class TP_SEO {
 	function __construct() {
-		if( defined( 'TP_ENV' ) && 'release' == TP_ENV )
+		if( ( defined( 'TP_ENV' ) && 'release' == TP_ENV ) || defined( 'TP_NOINDEX' ) && true === TP_NOINDEX )
 			update_option( 'blog_public', 0 );
+		else if( defined( 'TP_ENV' ) && 'master' == TP_ENV )
+			update_option( 'blog_public', 1 );
 	}
 } new TP_SEO;
