5a369e590ed8913025f872e03e783d30b70afe14
Harold Angenent <harold@intengames.com>
2014-05-08 12:04:30 +0200

    Better coding style

--- assets/inc/class-tp-seo.php
+++ assets/inc/class-tp-seo.php
@@ -7,7 +7,7 @@
 
 class TP_SEO {
 	function __construct() {
-		if( ( defined( 'TP_ENV' ) && 'release' == TP_ENV ) || defined( 'TP_NOINDEX' ) && true === TP_NOINDEX )
+		if( ( defined( 'TP_ENV' ) && 'release' == TP_ENV ) || ( defined( 'TP_NOINDEX' ) && true === TP_NOINDEX ) )
 			update_option( 'blog_public', 0 );
 		else if( defined( 'TP_ENV' ) && 'master' == TP_ENV )
 			update_option( 'blog_public', 1 );
