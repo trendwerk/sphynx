18ba081359a3110e1688fc84a23ff15e5aba26f3
Derrik Wolthuis <derrik@trendwerk.nl>
2014-06-18 16:44:15 +0200

    Working on implementing SASS

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 
 		/**
-		 * Combine files with concatenate 
+		 * Combine files with concatenate
 		 */
 
 		pkg: grunt.file.readJSON( 'package.json' ),
@@ -16,18 +16,43 @@ module.exports = function(grunt) {
 				],
 			dest: 'assets/js/all.js',
 			}
-		}
+		},
 
 		/**
-		 * Uglify 
+		 * Uglify files
 		 */
-		
-		// uglify: {
-		// 	build: {
-		// 		src: 'js/build/all.js',
-		// 		dest: 'js/build/all.min.js'
-		// 	}
-		// }
+
+		uglify: {
+			build: {
+				src: 'assets/js/all.js',
+				dest: 'assets/js/all.min.js'
+			}
+		},
+
+		/**
+		 * Uglify files
+		 */		
+
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				files: {
+					'sass.css': 'style.scss',
+				}
+			}
+		},
+
+		/**
+		 * Optimize images
+		 */
+
+		imageoptim: {
+			myTask: {
+				src: ['assets/img']
+			}
+		},
 
 	} );
 
@@ -35,13 +60,15 @@ module.exports = function(grunt) {
 	 * Load tasks
 	 */
 
-	grunt.loadNpmTasks( 'grunt-contrib-concat' );
-	// grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-imageoptim');
 
 	/**
 	 * Run tasks
 	 */
 
-	grunt.registerTask( 'default', ['concat'] );
+	grunt.registerTask( 'default', ['concat', 'uglify', 'sass', 'imageoptim'] );
 
 };
--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ function tp_enqueue_scripts() {
 	//Register & enqueue child scripts and styles
 	wp_enqueue_script( 'functions', get_stylesheet_directory_uri() . '/assets/js/functions.js', array( 'jquery' ) );
 	wp_enqueue_script( 'responsive', get_stylesheet_directory_uri() . '/assets/js/responsive.js', array( 'jquery' ) );
-	wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/style.less' );
+	wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/sass.css' );
 	
 	//Get jQuery from Google CDN
     wp_deregister_script( 'jquery' );
