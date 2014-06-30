c9c8b45052e1bfd755e0bd2947aaabf2e0c7a290
Derrik Wolthuis <derrik@trendwerk.nl>
2014-06-30 14:53:38 +0200

    Code consistency!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,21 @@
-module.exports = function(grunt) {
+module.exports = function( grunt ) {
 
-	grunt.initConfig({
+	grunt.initConfig( {
 
 		pkg: grunt.file.readJSON( 'package.json' ),
 
 		/**
 		 * Compile SASS to CSS 
-		 */		
-
+		 */	
 		sass: {
 			options: {
 				style: 'compressed'
 			},
 			dist: {
 				files: {
-					'assets/sass/output/style.css': 'assets/sass/style.scss',
+					'assets/sass/output/admin.css': 'assets/sass/admin.scss',
 					'assets/sass/output/editor.css': 'assets/sass/editor.scss',
-					'assets/sass/output/admin.css': 'assets/sass/admin.scss'
+					'assets/sass/output/style.css': 'assets/sass/style.scss'
 				}
 			}
 		},
@@ -24,35 +23,33 @@ module.exports = function(grunt) {
 		/**
 		 * Watch things
 		 */
-
-        watch: {
-            sass: {
-                files: [
-                		'assets/plugins/*',
-                		'assets/sass/*',
-                		'assets/sass/inc/*',
-                		'assets/sass/inc/lib/*',
-                		'asset/sass/output/*'],
-                tasks: ['sass'],
-                options: {
+		watch: {
+			sass: {
+				files: [
+					'assets/plugins/*',
+					'assets/sass/*',
+					'assets/sass/inc/*',
+					'assets/sass/inc/lib/*',
+					'assets/sass/output/*'
+				],
+				tasks: [ 'sass' ],
+				options: {
 					livereload: 35729
 				}
-            }
-        }
+			}
+		}
 
 	} );
 
 	/**
 	 * Load tasks
 	 */
-
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks( 'grunt-contrib-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
 	/**
 	 * Run tasks
 	 */
-
-	grunt.registerTask( 'default', ['sass', 'watch'] );
+	grunt.registerTask( 'default', [ 'sass', 'watch' ] );
 
 };
