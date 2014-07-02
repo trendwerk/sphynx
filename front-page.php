871b9a3209412ca25042445892545150d2cf58f8
Derrik Wolthuis <derrik@trendwerk.nl>
2014-07-02 12:04:45 +0200

    Made #sub-header part of layout rather than templates Fixed issue double h1 on front-page.php

--- front-page.php
+++ front-page.php
@@ -24,9 +24,9 @@
 		
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			
-				<h1 id="page-title">
+				<h2 id="page-title">
 					<?php the_title(); ?>
-				</h1>
+				</h2>
 				
 				<?php the_content(); ?>
 				
