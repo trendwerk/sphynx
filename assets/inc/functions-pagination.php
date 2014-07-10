a9b4acebd420627c901c3c233b467c9fb547e9b5
Harold Angenent <harold@trendwerk.nl>
2014-07-10 18:05:58 +0200

    Coding style

--- assets/inc/functions-pagination.php
+++ assets/inc/functions-pagination.php
@@ -8,11 +8,11 @@ function tp_pagination( $args = array() ) {
 	global $wp_query;
 
 	$pagination = paginate_links( wp_parse_args( $args, array(
-		'base'     => str_replace( PHP_INT_MAX, '%#%', esc_url( get_pagenum_link( PHP_INT_MAX ) ) ),
-		'current'  => max( 1, get_query_var( 'paged' ) ),
-		'total'    => $wp_query->max_num_pages,
-		'prev_text'    => __('Previous'),
-		'next_text'    => __('Next')
+		'base'      => str_replace( PHP_INT_MAX, '%#%', esc_url( get_pagenum_link( PHP_INT_MAX ) ) ),
+		'current'   => max( 1, get_query_var( 'paged' ) ),
+		'total'     => $wp_query->max_num_pages,
+		'prev_text' => __( 'Previous' ),
+		'next_text' => __( 'Next' )
 	) ) );
 
 	echo '<nav id="pager">' . $pagination . '</nav>';
