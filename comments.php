1ca1218ac28ac9c2aaa1455a20b0d79dee82f83f
Harold Angenent <harold@trendwerk.nl>
2014-07-11 14:56:59 +0200

    Fixes #262

--- comments.php
+++ comments.php
@@ -5,22 +5,28 @@
 
 <div id="comments">
 
-    <?php if ( have_comments() ) { ?>
-    
-        <h2 class="comments-title">
-            <?php printf( _n( 'One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'tp' ), get_comments_number(), '<span>' . get_the_title() . '</span>' ); ?>
-        </h2>
-        
-        <ol class="commentlist">
-            <?php 
-                wp_list_comments( array(
-    				'avatar_size' => 60,
-    			) );
+    <?php 
+        if ( have_comments() ) { 
             ?>
-        </ol>
-        
-    <?php } ?>
     
-    <?php comment_form(); ?>
+            <h2 class="comments-title">
+                <?php printf( _n( 'One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'tp' ), get_comments_number(), '<span>' . get_the_title() . '</span>' ); ?>
+            </h2>
+            
+            <ol class="commentlist">
+                <?php 
+                    wp_list_comments( array(
+        				'avatar_size' => 60,
+        			) );
+                ?>
+            </ol>
+        
+            <?php 
+        }
+
+        comment_form( array(
+            'comment_notes_after' => '',
+        ) );
+    ?>
     
 </div>
