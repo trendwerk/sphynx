850af68105ef7dbbf797fe2677d48099d41db091
Harold Angenent <harold@trendwerk.nl>
2014-10-03 17:45:40 +0200

    Better description

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Enqueue scripts and styles, register languages
+ * 
+ * @package TrendPress
+ * @see assets/inc/ for all specific functions
+ */
+
+/**
+ * Add theme support for featured images
+ */
+add_theme_support( 'post-thumbnails' );
+
+/**
+ * Add HTML5 theme support
+ */
+add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+
+/**
+ * Add theme support for RSS links
+ */
+add_theme_support( 'automatic-feed-links' );
+
+/**
+ * Add localization support
+ */
+load_theme_textdomain( 'tp', get_stylesheet_directory() . '/assets/lang' );
