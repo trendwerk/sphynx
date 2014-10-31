618574ae1d859b8d302f11e5a71a915869552a73
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2014-10-31 14:37:43 +0100

    Replaced $content_width with ‘Set embed size’ function

--- assets/inc/misc.php
+++ assets/inc/misc.php
@@ -9,7 +9,7 @@
  * Load editor styles
  */
 function tp_add_editor_style() {
-    add_editor_style( 'assets/sass/output/editor.css' );
+	add_editor_style( 'assets/sass/output/editor.css' );
 }
 add_action( 'init', 'tp_add_editor_style' );
 
@@ -17,46 +17,49 @@ add_action( 'init', 'tp_add_editor_style' );
  * Define editor styles
  */
 function tp_add_editor_styles( $settings ) {
-    $style_formats = array(
-    	array(
-    		'title'    => __( 'Button', 'tp' ),
-    		'selector' => 'a',
-    		'classes'  => 'button',
-    	),
-    	array(
-    		'title'    => __( 'Secondary button', 'tp' ),
-    		'selector' => 'a',
-    		'classes'  => 'button secondary',
-    	),
-    	array(
-    		'title'    => __( 'More link', 'tp' ),
-    		'selector' => 'a',
-    		'classes'  => 'more-link',
-    	),   
-    );
-    $settings['style_formats'] = json_encode( $style_formats );
+	$style_formats = array(
+		array(
+			'title'    => __( 'Button', 'tp' ),
+			'selector' => 'a',
+			'classes'  => 'button',
+		),
+		array(
+			'title'    => __( 'Secondary button', 'tp' ),
+			'selector' => 'a',
+			'classes'  => 'button secondary',
+		),
+		array(
+			'title'    => __( 'More link', 'tp' ),
+			'selector' => 'a',
+			'classes'  => 'more-link',
+		),   
+	);
+	$settings['style_formats'] = json_encode( $style_formats );
 
-    return $settings;
+	return $settings;
 }
 add_filter( 'tiny_mce_before_init', 'tp_add_editor_styles' );
 
 /**
- * Set content width
+ * Set embed width
  */
-if( ! isset( $content_width ) )
-    $content_width = 740;
+function tp_set_embed_width( $embed_size ) {
+	$embed_size['width'] = 720;
+	return $embed_size;
+}
+add_filter( 'embed_defaults', 'tp_set_embed_width' );
 
 /**
  * Set image sizes
  */
 function tp_set_image_sizes() {
-	update_option( 'thumbnail_size_w', 140 );
-	update_option( 'thumbnail_size_h', 140 );
-	update_option( 'medium_size_w', 300 );
+	update_option( 'thumbnail_size_w', 150 );
+	update_option( 'thumbnail_size_h', 150 );
+	update_option( 'medium_size_w', 340 );
 	update_option( 'medium_size_h', '' );
-	update_option( 'large_size_w', 600 );
+	update_option( 'large_size_w', 720 );
 	update_option( 'large_size_h', '' );
-    update_option( 'image_default_link_type', 'file' );
+	update_option( 'image_default_link_type', 'file' );
 }
 add_action( 'after_switch_theme', 'tp_set_image_sizes' );
 
@@ -74,7 +77,7 @@ add_theme_support( 'automatic-feed-links' );
  * Responsive video container
  */
 function tp_video_embed( $html ) {
-    return '<div class="video-container">' . $html . '</div>';
+	return '<div class="video-container">' . $html . '</div>';
 }
 add_filter( 'embed_oembed_html', 'tp_video_embed' );
 
@@ -89,7 +92,7 @@ function tp_modify_profile( $media ) {
 	$media['facebook']   = __( 'Facebook profile URL', 'tp' );
 	$media['linkedin']   = __( 'LinkedIn profile URL', 'tp' );
 	$media['googleplus'] = __( 'Google+ profile URL', 'tp' );
-    $media['twitter']    = __( 'Twitter @username', 'tp' );
+	$media['twitter']    = __( 'Twitter @username', 'tp' );
 	
 	return $media;
 }
