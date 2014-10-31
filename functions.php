3087e5e782823c2175f8ca3988b64159a898002c
Harold Angenent <harold@trendwerk.nl>
2014-10-31 14:52:45 +0100

    Moved includer to child theme

--- functions.php
+++ functions.php
@@ -7,13 +7,8 @@
 
 /**
  * Include PHP files in /assets/inc/
- * Include PHP files in trendpress-child/assets/inc/
  */
-
-include_once( 'assets/inc/class-tp-includer.php' );
-
 new TP_Includer( dirname( __FILE__ ) . '/assets/inc/' );
-new TP_Includer( get_stylesheet_directory() . '/assets/inc/' );
 
 /**
  * Load the textdomain 'tp'
