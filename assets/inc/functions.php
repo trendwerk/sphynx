32c60e03397096b98d37e39183c96666e6308972
Harold Angenent <harold@trendwerk.nl>
2014-10-31 18:14:31 +0100

    Moved some functions to child

--- assets/inc/functions.php
+++ assets/inc/functions.php
@@ -6,32 +6,6 @@
  */
 
 /**
- * Debug a variable
- * 
- * @param mixed $var
- */
-function dbg( $var ) {
-	echo '<pre>';
-	print_r( $var );
-	echo '</pre>';
-}
-
-/**
- * Convert a string to an URL (Add http:// if necessary)
- *
- * @param string $url
- */
-function tp_maybe_add_http( $url ) {
-	if( ! $url ) 
-		return;
-	
-	if( ! strstr( $url, 'http://' ) && ! strstr( $url, 'https://' ) )
-		$url = 'http://' . $url;
-	
-	return $url;
-}
-
-/**
  * A function for a custom excerpt length
  *
  * @param int $length The length of the excerpt (in words)
@@ -56,24 +30,4 @@ function tp_the_excerpt( $length=55, $more='&hellip;', $content='' ) {
 	remove_filter( 'excerpt_length', $func['length'] );
 	
 	echo $excerpt;
-}
-
-/**
- * Pagination
- *
- * @param array $args Additional arguments
- */
-function tp_pagination( $args = array() ) {
-	global $wp_query;
-
-	$pagination = paginate_links( wp_parse_args( $args, array(
-		'base'      => str_replace( PHP_INT_MAX, '%#%', esc_url( get_pagenum_link( PHP_INT_MAX ) ) ),
-		'current'   => max( 1, get_query_var( 'paged' ) ),
-		'total'     => $wp_query->max_num_pages,
-		'prev_text' => __( 'Previous', 'tp' ),
-		'next_text' => __( 'Next', 'tp' )
-	) ) );
-
-	if( 0 < strlen( $pagination ) )
-		echo '<nav id="pager">' . $pagination . '</nav>';
 }
