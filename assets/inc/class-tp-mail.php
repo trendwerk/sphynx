6ff2fd6b7dec419ed409c00497f60d5e298ce213
Harold Angenent <harold@trendwerk.nl>
2014-10-31 15:24:08 +0100

    Moving SMTP to plugin

--- assets/inc/class-tp-mail.php
+++ assets/inc/class-tp-mail.php
@@ -9,9 +9,6 @@ class TP_Mail {
 	function __construct() {
 		if( ! defined( 'TP_ENV' ) || 'develop' == TP_ENV || 'release' == TP_ENV )
 			add_filter( 'wp_mail', array( $this, 'send_to' ) );
-
-		if( defined( 'TP_SMTP' ) && true === TP_SMTP )
-			add_action( 'phpmailer_init', array( $this, 'smtp' ) );
 	}
 
 	/**
@@ -29,29 +26,5 @@ class TP_Mail {
 
 		return $mail;
 	}
-
-	/**
-	 * Send e-mail via SMTP
-	 */
-	function smtp( $mailer ) {
-		if( ! is_email( TP_SMTP_FROM ) || ! defined( 'TP_SMTP_HOST' ) )
-			return;
-
-		$mailer->Mailer = 'smtp';
-		$mailer->From = TP_SMTP_FROM;
-		$mailer->FromName = TP_SMTP_FROM_NAME;
-
-		$mailer->Sender = $mailer->From;
-		$mailer->AddReplyTo( $mailer->From, $mailer->FromName );
-
-		$mailer->Host = TP_SMTP_HOST;
-		$mailer->SMTPSecure = TP_SMTP_SECURE;
-		$mailer->Port = TP_SMTP_PORT;
-
-		$mailer->SMTPAuth = TP_SMTP_AUTH;
-		if( $mailer->SMTPAuth ) {
-			$mailer->Username = TP_SMTP_USERNAME;
-			$mailer->Password = TP_SMTP_PASSWORD;
-		}
-	}
+	
 } new TP_Mail;
