c017a0bfd3097af1303790e27dcefbe04fdda506
Harold Angenent <harold@trendwerk.nl>
2015-02-19 17:39:24 +0100

    Order functions alphabetically

--- assets/inc/template-tags.php
+++ assets/inc/template-tags.php
@@ -1,29 +1,9 @@
 <?php
 /**
- * Contains helpers and template tags. No hooks!
+ * Contains helpers and template tags.
  *
  * @package TrendPress
  */
-
-/**
- * Pagination
- *
- * @param array $args Additional arguments
- */
-function tp_pagination( $args = array() ) {
-	global $wp_query;
-
-	$pagination = paginate_links( wp_parse_args( $args, array(
-		'base'      => str_replace( PHP_INT_MAX, '%#%', esc_url( get_pagenum_link( PHP_INT_MAX ) ) ),
-		'current'   => max( 1, get_query_var( 'paged' ) ),
-		'total'     => $wp_query->max_num_pages,
-		'prev_text' => __( 'Previous', 'tp' ),
-		'next_text' => __( 'Next', 'tp' )
-	) ) );
-
-	if( 0 < strlen( $pagination ) )
-		echo '<nav id="pager">' . $pagination . '</nav>';
-}
 
 /**
  * Debug a variable
@@ -49,4 +29,24 @@ function tp_maybe_add_http( $url ) {
 		$url = 'http://' . $url;
 	
 	return $url;
+}
+
+/**
+ * Pagination
+ *
+ * @param array $args Additional arguments
+ */
+function tp_pagination( $args = array() ) {
+	global $wp_query;
+
+	$pagination = paginate_links( wp_parse_args( $args, array(
+		'base'      => str_replace( PHP_INT_MAX, '%#%', esc_url( get_pagenum_link( PHP_INT_MAX ) ) ),
+		'current'   => max( 1, get_query_var( 'paged' ) ),
+		'total'     => $wp_query->max_num_pages,
+		'prev_text' => __( 'Previous', 'tp' ),
+		'next_text' => __( 'Next', 'tp' )
+	) ) );
+
+	if( 0 < strlen( $pagination ) )
+		echo '<nav id="pager">' . $pagination . '</nav>';
 }
