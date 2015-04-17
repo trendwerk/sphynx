d6531aad195a5822e269bf6c856d13f243c01e5d
Harold Angenent <harold@trendwerk.nl>
2015-04-17 13:58:12 +0200

    Add support for title tag

--- header.php
+++ header.php
@@ -9,8 +9,6 @@
 
 	<head>
 
-		<title><?php wp_title( '-' ); ?></title>
-
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
