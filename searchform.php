b407fd8d599489d3b839e1fabbe6786348c11658
Luuk de Vlieger <luuk@iMac-van-Sjoerd.fritz.box>
2015-05-15 13:50:11 +0200

    Updated search icon

--- searchform.php
+++ searchform.php
@@ -2,7 +2,9 @@
 
 	<form action="<?php echo home_url(); ?>" method="get">
 		<input type="text" name="s" placeholder="<?php _e( 'Search', 'tp' ); ?>&hellip;" />
-		<button type="submit">&#xf002;</button>
+		<button type="submit">
+			<span id="glass">
+		</button>
 	</form>
 
 </div>
