8c439bc6c224f037d2eae8f0d1993c2206d856b9
Harold Angenent <harold@trendwerk.nl>
2015-05-22 14:42:10 +0200

    Front page

--- front-page.php
+++ front-page.php
@@ -1,44 +1,6 @@
 <?php
-get_header();
-?>
+$context = Timber::get_context();
+$context['post'] = new TimberPost();
+$context['subheader'] = true;
 
-<header id="sub-header" class="container">
-
-	<div class="container-inner">
-
-		<h1>
-			<?php bloginfo( 'name' ); ?>
-		</h1>
-		
-		<h2>
-			<?php bloginfo( 'description' ); ?>
-		</h2>
-
-	</div>
-
-</header>
-
-<section id="main" class="container">
-
-	<div class="container-inner">
-	
-		<article id="content">
-		
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			
-				<h2>
-					<?php the_title(); ?>
-				</h2>
-				
-				<?php the_content(); ?>
-				
-			<?php endwhile; endif; ?>	
-					
-		</article>
-		
-	</div>
-	
-</section>
-
-<?php
-get_footer();
+Timber::render( 'page.twig', $context );
