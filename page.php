7f77f946ead85f82ec20e432acc237c86d7bccca
Harold Angenent <harold@trendwerk.nl>
2015-05-22 11:26:50 +0200

    Remove pagination support for multipages
    Not yet elegant enough in Timber

--- page.php
+++ page.php
@@ -1,12 +1,5 @@
 <?php
 $context = Timber::get_context();
-
 $context['post'] = new TimberPost();
-$context['post']->pagination = wp_link_pages( array(
-	'before'         => '<nav class="pages">',
-	'after'          => '</nav>',
-	'next_or_number' => 'next',
-	'echo'           => false,
-) );
 
 Timber::render( 'page.twig', $context );
