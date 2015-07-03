a73ac957f98a61070ec9a353da6ac6b44f775ded
Harold Angenent <harold@trendwerk.nl>
2015-07-03 14:59:45 +0200

    PSR Enqueue

--- assets/includes/class-tp-enqueue.php
+++ assets/includes/class-tp-enqueue.php
@@ -1,41 +1,48 @@
 <?php
 /**
  * Enqueue styles and scripts
- *
- * @package TrendPress
  */
 
-class TP_Enqueue {
-	function __construct() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'frontend' ) );
-	}
+namespace Trendwerk\TrendPress;
 
-	/**
-	 * Enqueue styles and scripts for front-end
-	 */
-	function frontend() {
-		/**
-		 * Core
-		 */
-		wp_enqueue_script( 'comment-reply' );
+final class Enqueue
+{
+    public function __construct()
+    {
+        add_action('wp_enqueue_scripts', array($this, 'frontend'));
+    }
 
-		/**
-		 * Scripts
-		 */
-		wp_enqueue_script( 'functions', get_template_directory_uri() . '/assets/scripts/output/functions.js', array( 'jquery', 'fancybox' ) );
-		wp_enqueue_script( 'responsive', get_template_directory_uri() . '/assets/scripts/output/responsive.js', array( 'jquery' ) );
-		wp_enqueue_script( 'fancybox', get_template_directory_uri() . '/assets/scripts/lib/fancybox/jquery.fancybox.js', array( 'jquery' ) );
+    /**
+     * Enqueue styles and scripts for front-end
+     */
+    public function frontend()
+    {
+        $assets = get_template_directory_uri() . '/assets';
 
-		/**
-		 * Styles
-		 */
-		wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/styles/output/style.css' );
-		wp_enqueue_style( 'fancybox', get_template_directory_uri() . '/assets/scripts/lib/fancybox/jquery.fancybox.css' );
+        /**
+         * Core
+         */
+        wp_enqueue_script('comment-reply');
 
-		/**
-		 * jQuery from Google's CDN
-		 */
-		wp_deregister_script( 'jquery' );
-		wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js' );
-	}
-} new TP_Enqueue;
+        /**
+         * Scripts
+         */
+        wp_enqueue_script('functions', $assets . '/scripts/output/functions.js', array('jquery', 'fancybox'));
+        wp_enqueue_script('responsive', $assets . '/scripts/output/responsive.js', array('jquery'));
+        wp_enqueue_script('fancybox', $assets . '/scripts/lib/fancybox/jquery.fancybox.js', array('jquery'));
+
+        /**
+         * Styles
+         */
+        wp_enqueue_style('style', $assets . '/styles/output/style.css');
+        wp_enqueue_style('fancybox', $assets . '/scripts/lib/fancybox/jquery.fancybox.css');
+
+        /**
+         * jQuery from Google's CDN
+         */
+        wp_deregister_script('jquery');
+        wp_enqueue_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
+    }
+}
+
+new Enqueue;
