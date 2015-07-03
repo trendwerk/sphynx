393e24976de7b62b5831b1c7fa32c5031ef7f867
Harold Angenent <harold@trendwerk.nl>
2015-07-03 14:45:09 +0200

    PSR Media

---
+++ assets/includes/media.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Everything that has to do with media
+ */
+
+namespace Trendwerk\TrendPress;
+
+final class Media
+{
+    public function __construct()
+    {
+        add_action('after_switch_theme', array( $this, 'defaults' ));
+        add_filter('embed_oembed_html', array( $this, 'videoEmbed' ));
+    }
+
+    /**
+     * Set default image sizes
+     */
+    public function defaults()
+    {
+        update_option('thumbnail_size_w', 150);
+        update_option('thumbnail_size_h', 150);
+
+        update_option('medium_size_w', 340);
+        update_option('medium_size_h', '');
+
+        update_option('large_size_w', 720);
+        update_option('large_size_h', '');
+        
+        update_option('image_default_link_type', 'file');
+    }
+
+    /**
+     * Responsive video container
+     */
+    public function videoEmbed($html)
+    {
+        return '<div class="video-container">' . $html . '</div>';
+    }
+}
+
+new Media;
