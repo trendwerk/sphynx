0f8c6d0281fa78b41234c64646eb6bb54bf56aa0
Harold Angenent <harold@trendwerk.nl>
2015-07-03 14:48:08 +0200

    PSR Includer (still needs autoloader)

--- assets/includes/class-tp-includer.php
+++ assets/includes/class-tp-includer.php
@@ -1,26 +1,30 @@
 <?php
 /**
  * Include an entire folder of files
- *
- * @package TrendPress
  */
- 
-class TP_Includer {
-	function __construct( $folder ) {
-		if( is_dir( $folder ) ) {
-			foreach( scandir( $folder ) as $inc ) {
 
-				if( '.' == substr( $inc, 0, 1 ) ) 
-					continue;
-				
-				if( '.php' != substr( $inc, -4 ) ) 
-					continue;
+namespace Trendwerk\TrendPress;
 
-				$inc = $folder . $inc;
+final class Includer
+{
+    public function __construct($folder)
+    {
+        if (is_dir($folder)) {
+            foreach (scandir($folder) as $inc) {
+                if ('.' == substr($inc, 0, 1)) {
+                    continue;
+                }
+                
+                if ('.php' != substr($inc, -4)) {
+                    continue;
+                }
 
-				if( is_readable( $inc ) )
-					include_once( $inc );
-			}
-		}
-	}
+                $inc = $folder . $inc;
+
+                if (is_readable($inc)) {
+                    include_once($inc);
+                }
+            }
+        }
+    }
 }
