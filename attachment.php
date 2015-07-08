dc8d692ccd5bcddc8185d66bea0c382b20211a49
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2015-07-08 14:11:07 +0200

    Added linters

--- attachment.php
+++ attachment.php
@@ -1,7 +1,3 @@
 <?php
-<<<<<<< HEAD
-wp_redirect( wp_get_attachment_url(), 301 );
-=======
 wp_redirect(wp_get_attachment_url(), 301);
->>>>>>> origin/psr
 die();
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
+var scsslint = require('gulp-scss-lint');
+var coffeelint = require('gulp-coffeelint');
 var phpcs = require('gulp-phpcs');
 
 /**
@@ -29,19 +31,42 @@ gulp.task('coffee', function() {
 });
 
 /**
+ * Lint Sass
+ */
+gulp.task('scss-lint', function() {
+  gulp.src('assets/styles/**/*.scss')
+    .pipe(scsslint({
+      'config': 'lint.yml',
+    }));
+});
+
+/**
+ * Lint CoffeeScript
+ */
+gulp.task('coffeelint', function () {
+  gulp.src('assets/scripts/**/*.coffee')
+    .pipe(coffeelint())
+    .pipe(coffeelint.reporter())
+});
+
+/**
+ * lint PHP
+ */
+gulp.task('phpcs', function() {
+  gulp.src(['**/*.php', '!node_modules/**/*.*'])
+    .pipe(phpcs({
+      bin: '~/.composer/vendor/bin/phpcs',
+      standard: 'PSR2',
+      warningSeverity: 0
+    }))
+    .pipe(phpcs.reporter('log'));
+});
+
+/**
  * Watch things
  */
 gulp.task('default',function() {
-  gulp.watch('assets/styles/**/*.scss',['sass']);
-  gulp.watch('assets/scripts/**/*.coffee',['coffee']);
-
-  return gulp.src(['**/*.php', '!node_modules/**/*.*'])
-      // Validate files using PHP Code Sniffer
-      .pipe(phpcs({
-          bin: '~/.composer/vendor/bin/phpcs',
-          standard: 'PSR2',
-          warningSeverity: 0
-      }))
-      // Log all problems that was found
-      .pipe(phpcs.reporter('log'));
+  gulp.watch('assets/styles/**/*.scss',['sass', 'scss-lint']);
+  gulp.watch('assets/scripts/**/*.coffee',['coffee', 'coffeelint']);
+  gulp.watch('**/*.php',['phpcs']);
 });
