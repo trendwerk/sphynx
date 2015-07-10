689353544cedf3f13334f2f8da6b390af60af0ba
Harold Angenent <harold@trendwerk.nl>
2015-07-10 14:05:14 +0200

    Refrain from using fn()
    #452

--- search.php
+++ search.php
@@ -2,6 +2,7 @@
 $context = Timber::get_context();
 $context['have_posts'] = have_posts();
 
+$context['search_query'] = get_search_query();
 $context['pagination'] = Trendwerk\TrendPress\pagination();
 
 Timber::render('search.twig', $context);
