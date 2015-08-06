d4c0cee323dfbaca624ff6df5b402759e9b73a99
Harold Angenent <harold@trendwerk.nl>
2015-08-06 18:35:01 +0200

    Replace mobile ID selectors by classes

--- assets/scripts/output/responsive.js
+++ assets/scripts/output/responsive.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('#mobile-navigation, #mobile-search').click(function() {
+  $('.mobile-navigation, .mobile-search').click(function() {
     return $($(this).data('toggle')).slideToggle('fast', function() {
       if (!$(this).is(':visible')) {
         return $(this).css({
@@ -8,10 +8,10 @@ $(function() {
       }
     });
   });
-  $('#mobile-navigation').click(function() {
+  $('.mobile-navigation').click(function() {
     return $('.burger', this).toggleClass('active');
   });
-  return $('#mobile-search').click(function() {
+  return $('.mobile-search').click(function() {
     $($(this).data('toggle')).find('input[type="text"]').focus();
     return $('.glass', this).toggleClass('active');
   });
--- assets/scripts/responsive.coffee
+++ assets/scripts/responsive.coffee
@@ -1,14 +1,14 @@
 $ ->
 
   # Toggle navigation search
-  $( '#mobile-navigation, #mobile-search' ).click ->
+  $( '.mobile-navigation, .mobile-search' ).click ->
     $( $( this ).data 'toggle' ).slideToggle 'fast', ->
       if ! $( this ).is ':visible'
         $( this ).css 'display': ''
 
-  $( '#mobile-navigation' ).click ->
+  $( '.mobile-navigation' ).click ->
     $( '.burger', this ).toggleClass( 'active' )
 
-  $( '#mobile-search' ).click ->
+  $( '.mobile-search' ).click ->
     $( $( this ).data 'toggle' ).find( 'input[type="text"]' ).focus()
     $( '.glass', this ).toggleClass( 'active' )
