b790687ec6efc939e799890c00af80361cbca534
Harold Angenent <harold@trendwerk.nl>
2015-09-04 10:47:24 +0200

    Commit functions.php
    #459

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Autoload classes
+ */
+include_once('lib/autoload.php');
+
+/**
+ * Template tags
+ */
+include_once('lib/template-tags.php');
+
+/**
+ * Initialize
+ */
+include_once('lib/init.php');
