2520dbb942d56de9644f75bc8364c302402170d9
Harold Angenent <harold@trendwerk.nl>
2015-10-02 14:09:50 +0200

    Replace pagination by Timber
    #450

--- functions.php
+++ functions.php
@@ -5,11 +5,6 @@
 include_once('lib/autoload.php');
 
 /**
- * Template tags
- */
-include_once('lib/template-tags.php');
-
-/**
  * Initialize
  */
 include_once('lib/init.php');
--- search.php
+++ search.php
@@ -3,6 +3,8 @@ $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 
 $context['search_query'] = get_search_query();
-$context['pagination'] = Trendwerk\TrendPress\pagination();
+$context['pagination'] = Timber::get_pagination(array(
+    'mid_size' => 2,
+));
 
 Timber::render('search.twig', $context);
