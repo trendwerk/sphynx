2edf655edbe403dee922e83674c43d7382c05433
Harold Angenent <harold@trendwerk.nl>
2015-10-02 13:46:12 +0200

    Create TimberHelper for Menus: SoC
    #449

--- lib/classes/Menus.php
+++ lib/classes/Menus.php
@@ -10,7 +10,6 @@ final class Menus
     public function __construct()
     {
         add_action('init', array($this, 'register'));
-        add_filter('timber_context', array($this, 'timber'));
     }
 
     /**
@@ -20,18 +19,5 @@ final class Menus
     {
         register_nav_menu('main', __('Main navigation', 'tp'));
         register_nav_menu('footer', __('Footer', 'tp'));
-    }
-
-    /**
-     * Make global menu's available to Timber
-     */
-    public function timber($context)
-    {
-        $context['menus'] = array(
-            'main'   => new \TimberMenu('main'),
-            'footer' => new \TimberMenu('footer'),
-        );
-
-        return $context;
     }
 }
