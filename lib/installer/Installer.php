c5b4e88878229a411f8bf94292084138f68e04ac
Harold Angenent <harold@trendwerk.nl>
2015-10-02 17:20:30 +0200

    Rename namespaces

--- lib/installer/Installer.php
+++ lib/installer/Installer.php
@@ -12,6 +12,7 @@ final class Installer
     public static function setNamespace($event)
     {
         $defaultNamespace = __NAMESPACE__;
+        $root = dirname(dirname(__DIR__));
         $io = $event->getIO();
 
         if (! $io->isInteractive()) {
@@ -25,13 +26,52 @@ final class Installer
             return 1;
         }
 
-        self::renameNamespace($namespace);
+        $changedFiles = self::renameNamespaceInDir($root, $defaultNamespace, $namespace);
+
+        $io->write('<info>Renamed namespace in ' . $changedFiles . ' files</info>');
 
         return 1;
     }
 
-    public static function renameNamespace($namespace)
+    public static function renameNamespaceInDir($dir, $fromNamespace, $toNamespace)
     {
-        $root = dirname(dirname(__DIR__));
+        $changedFiles = 0;
+        $files = scandir($dir);
+
+        if (count($files) == 0) {
+            return;
+        }
+
+        foreach ($files as $file) {
+            $extension = end((explode('.', $file)));
+
+            if (in_array($file, array('.', '..', 'node_modules', 'vendor'))) {
+                continue;
+            }
+
+            if ($extension == 'php') {
+                $changed = self::renameNamespaceInFile($dir . '/' . $file, $fromNamespace, $toNamespace);
+
+                if ($changed) {
+                    $changedFiles++;
+                }
+            } else if(is_dir( $dir . '/' . $file )) {
+                $changedFiles += self::renameNamespaceInDir($dir . '/' . $file, $fromNamespace, $toNamespace);
+            }
+        }
+
+        return $changedFiles;
+    }
+
+    public static function renameNamespaceInFile($file, $fromNamespace, $toNamespace) {
+        $_contents = $contents = file_get_contents($file);
+        $contents = str_replace($fromNamespace, $toNamespace, $contents);
+
+        if ($contents != $_contents) {
+            file_put_contents($file, $contents);
+            return true;
+        }
+
+        return false;
     }
 }
