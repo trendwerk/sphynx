5fe07cf65c4f9262363eafcfce5a6bf4406bc9d0
Harold Angenent <harold@trendwerk.nl>
2016-03-10 17:31:54 +0100

    Implement toggle-navigation()

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,8 @@ var files = {
   concat: {
     coffee: [
       'bower_components/jquery/dist/jquery.js',
-      'bower_components/fancybox/source/jquery.fancybox.js'
+      'bower_components/fancybox/source/jquery.fancybox.js',
+      'bower_components/toggle-navigation/toggle-navigation.js'
     ]
   }
 };
