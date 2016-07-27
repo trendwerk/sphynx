3ee1a2fb51213147043162cf90683e55dcbae361
Harold Angenent <harold@trendwerk.nl>
2016-07-27 10:06:20 +0200

    Replace wp_list_comments by Timber output (template)

--- comments.php
+++ comments.php
@@ -8,11 +8,6 @@ if (post_password_required()) {
 $context = Timber::get_context();
 $context['post'] = new Post();
 
-$context['comment_list'] = wp_list_comments(array(
-    'avatar_size' => 60,
-    'echo'        => false,
-));
-
 $context['comment_pages'] = paginate_comments_links(array(
     'next_text' => __('Next', 'tp'),
     'prev_text' => __('Previous', 'tp'),
