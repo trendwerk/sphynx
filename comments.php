1f0255f7b56ed9a5111d3b240e822ea19261fcd3
Harold Angenent <harold@trendwerk.nl>
2016-07-27 13:56:02 +0200

    Remove comment pagination

--- comments.php
+++ comments.php
@@ -8,10 +8,4 @@ if (post_password_required()) {
 $context = Timber::get_context();
 $context['post'] = new Post();
 
-$context['comment_pages'] = paginate_comments_links(array(
-    'next_text' => __('Next', 'tp'),
-    'prev_text' => __('Previous', 'tp'),
-    'echo'      => false,
-));
-
 Timber::render('partials/comments.twig', $context);
