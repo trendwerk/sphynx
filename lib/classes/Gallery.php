06c58c77844722f0cd50a9ed7d90fee05c3b3f44
Luuk <info@luuk.site>
2016-10-27 17:58:35 +0200

    Gallery as twig partial

--- lib/classes/Gallery.php
+++ lib/classes/Gallery.php
@@ -16,13 +16,17 @@ final class Gallery
 
     public function add()
     {
-        $items = \Timber::get_posts([
+        $context['gallery'] = array_map(function ($item) {
+            return new \TimberImage($item->ID);
+        }, get_posts([
             'post_mime_type' => 'image',
             'post_parent'    => get_the_ID(),
             'post_status'    => 'inherit',
             'post_type'      => 'attachment',
             'order'          => 'ASC',
             'orderby'        => 'menu_order ID',
-        ]);
+        ]));
+
+        return \Timber::fetch('partials/gallery.twig', $context);
     }
 }
