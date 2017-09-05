8b804eb5a4c9ee43b92f339dddd92c20555448e5
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-09-05 21:56:06 +0200

    :zap: Remove medium_large image size
    Fixes #781

--- lib/classes/Media.php
+++ lib/classes/Media.php
@@ -17,6 +17,9 @@ final class Media
         update_option('medium_size_w', 360);
         update_option('medium_size_h', '');
 
+        update_option('medium_large_size_w', 0);
+        update_option('medium_large_size_h', 0);
+
         update_option('large_size_w', 840);
         update_option('large_size_h', '');
 
