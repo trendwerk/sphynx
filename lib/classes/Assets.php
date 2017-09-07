f1770d2ec1714b64111027e9f0f761e9fa450d42
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-09-07 15:35:12 +0200

    :hammer: Remove temporary variable in Assets.php

--- lib/classes/Assets.php
+++ lib/classes/Assets.php
@@ -10,22 +10,19 @@ final class Assets
 
     public function frontend()
     {
-        $assetsPath = get_stylesheet_directory();
-        $assetsUri = get_stylesheet_directory_uri();
-
         /**
          * Style
          */
         $stylePath = '/styles/dist/main.css';
-        $styleModTime = filemtime($assetsPath . $stylePath);
+        $styleModTime = filemtime(get_stylesheet_directory() . $stylePath);
         wp_enqueue_style('fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,600');
-        wp_enqueue_style('main', $assetsUri . $stylePath, null, $styleModTime);
+        wp_enqueue_style('main', get_stylesheet_directory() . $stylePath, null, $styleModTime);
 
         /**
          * Script
          */
         $scriptPath = '/scripts/dist/all.js';
-        $scriptModTime = filemtime($assetsPath . $scriptPath);
-        wp_enqueue_script('main', $assetsUri . $scriptPath, null, $scriptModTime, true);
+        $scriptModTime = filemtime(get_stylesheet_directory() . $scriptPath);
+        wp_enqueue_script('main', get_stylesheet_directory() . $scriptPath, null, $scriptModTime, true);
     }
 }
